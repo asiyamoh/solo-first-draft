cf3369acdf6e5c2555da1c09a405b5d545e52158
Emma Stout <emma@primeacademy.io>
2023-07-25 15:59:48 -0500

    add lecture notes

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 // This route *should* return the logged in users pets
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('/pet GET route');
+    // Is the user logged in?
     console.log('is authenticated?', req.isAuthenticated());
+    // Who is the user?
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pets"`;
-    pool.query(queryText).then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log(error);
-        res.sendStatus(500);
-    });
+
+    // if (req.isAuthenticated()) {
+        let queryText = `SELECT * FROM "pets" WHERE "user_id" = $1`;
+        pool.query(queryText, [req.user.id]).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    // }
+    // else {
+        // res.sendStatus(403);
+    // }
 });
 
 // This route *should* add a pet for the logged in user
@@ -23,7 +32,7 @@ router.post('/', (req, res) => {
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
     res.sendStatus(200);
-    
+
 });
 
 module.exports = router;
